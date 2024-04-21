83a554cf6998c8869d9c8e630d80c7d1f2b38332
kabishbrt <kabishbrt@gmail.com>
2024-04-22 00:25:31 +0545

    >

--- src/app/components/database/index.ts
+++ src/app/components/database/index.ts
@@ -4,6 +4,6 @@ const globalPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
+/*if (process.env.NODE_ENV !== "production")*/ globalPrisma.prisma = prisma;
 
-export default prisma;
+export default prisma;  
