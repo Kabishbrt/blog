024cb0884ff1da3e8d97366ed5fb1575615ad3db
kabishbrt <kabishbrt@gmail.com>
2024-04-22 00:18:33 +0545

    fix error

---
+++ src/app/api/blog-post/get-all-posts/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/app/components/database";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const getAllBlogPosts = await prisma.post.findMany();
+    console.log(getAllBlogPosts)
+    
+    if (getAllBlogPosts && getAllBlogPosts.length) {
+      return NextResponse.json({
+        success: true,
+        data: getAllBlogPosts,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to fetch blog posts. Please try again",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong ! Please try again",
+    });
+  }
+}
